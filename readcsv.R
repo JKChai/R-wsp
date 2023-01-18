faab279222325c4ce1514e76a32c1c5bee205308
JKChai <chaijinkun@gmail.com>
2023-01-17 20:38:26 -0600

    Add old files

---
+++ readcsv.R
@@ -0,0 +1,7 @@
+read_csv = read.csv('C:\\Users\\chai8\\source\\repos\\Assets\\AUDI_SEARCH_US_All Search Accounts_Dec5.csv', sep="\t", header=TRUE, skipNul=TRUE)
+
+head(read_csv)
+
+colnames(read_csv)
+
+str(read_csv)
---
+++ stack-4.R
@@ -0,0 +1,5 @@
+names <- c('A','B','C','D','A','C','A')
+cols <- c(3,4,5,6,4,5,2)
+
+df <- data.frame(names, cols)
+print(unique(df[1]))
